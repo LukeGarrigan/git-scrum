79a5e31bc272fc3d485f4a5a23d9f83cfde76004
Luke <Lukegarrigan8@gmail.com>
2020-10-01 08:22:36 +0100

    Added a buffer, so it finds the last time you did some works then adds a 23 hour buffer on to that

---
+++ branch.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const moment = require('moment');
+module.exports = class Branch {
+
+    constructor(fileLocation) {
+        this.fileLocation = fileLocation;
+        this.timeSinceWorkedOn = this.calcTimeSinceWorkedOn();
+    }
+
+    calcTimeSinceWorkedOn() {
+        const fileStats = fs.statSync(this.fileLocation);
+        return moment().diff(fileStats.mtimeMs, 'hours') 
+    }
+ 
+    readFile() {
+        return fs.readFileSync(this.fileLocation, 'utf-8');
+    }
+}
--- index.js
+++ index.js
@@ -1,24 +1,45 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const moment = require('moment');
 const Commit = require('./commit');
-const currentFolder = fs.readdirSync(__dirname);
-allDirs = currentFolder.filter(f => fs.lstatSync(f).isDirectory())
-allDirs = getFoldersWithGitRepo(allDirs);
+const Branch = require('./branch');
+const colors = require('colors');
 
+const CWD = process.cwd();
+const currentFolder = fs.readdirSync(CWD);
+
+allDirs = currentFolder.filter(f => fs.lstatSync(`${CWD}\\${f}`).isDirectory());
+
+if (allDirs.length == 0) {
+    console.log(colors.red('There are no folders in this directory'));
+    return;
+}
+
+allDirs = getFoldersWithGitRepo(allDirs);
+if (allDirs.length == 0) {
+    console.log(colors.red('There are no repositories in any of the folders in this directory'));
+    return;
+}
+
+let hoursSinceLastWorked = Infinity;
 for (const dir of allDirs) {
-    console.log('Project', dir);
-    const branches = fs.readdirSync(`${__dirname}\\${dir}\\.git\\logs\\refs\\heads`);
+    console.log(colors.trap('-------------------------------------------------------------------------------------------------'));
+    console.log(colors.cyan(dir));
+    const branches = fs.readdirSync(`${CWD}\\${dir}\\.git\\logs\\refs\\heads`);
 
     for (let branchName of branches) {
-        const branchFile = fs.readFileSync(`${__dirname}\\${dir}\\.git\\logs\\refs\\heads\\${branchName}`, 'utf-8');
-        const commits = getLatestCommits(branchFile, branchName);
+
+        const branch = new Branch(`${CWD}\\${dir}\\.git\\logs\\refs\\heads\\${branchName}`);
+        if (branch.timeSinceWorkedOn < hoursSinceLastWorked) {
+            hoursSinceLastWorked = branch.timeSinceWorkedOn;
+        }
+        const commits = getLatestCommits(branch.readFile(), branchName);
         outputCommits(commits);
     }
-    
 }
 
 function getFoldersWithGitRepo(allDirs) {
-    return allDirs.filter(d => fs.readdirSync(`${__dirname}\\${d}`).includes('.git'));
+    return allDirs.filter(d => fs.readdirSync(`${CWD}\\${d}`).includes('.git'));
 }
 
 function getLatestCommits(branchFile, branchName) {
@@ -28,7 +49,7 @@ function getLatestCommits(branchFile, branchName) {
     let latestCommits = [];
     for (let line of lines) {
         let commit = new Commit(line, branchName);
-        if (moment().diff(commit.date, 'hours') < 24) {
+        if (moment().diff(commit.date, 'hours') < (hoursSinceLastWorked + 23)) {
             latestCommits.push(commit);
         }
     }
