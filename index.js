4699c4e01a07d8eb5fd53d0ed84b8c83234ea507
KARASZI István <ikaraszi@gmail.com>
2022-10-27 11:08:15 +0200

    Add option to fetch commits from specific date

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 #!/usr/bin/env node
 
-const colors = require('colors');
+const { Command, Option } = require('commander');
 const process = require('process');
-
 const { displayLatestCommits } = require('./main');
+
+const version = require('./package.json').version;
+
+const date = require('./date');
+
+const sinceOption = (option) => {
+    return new Option(`--${option} <date>`, 'Show work after a specific date').argParser(date);
+};
+
+const program = new Command();
+
+program
+    .name('git-scrum')
+    .version(version)
+    .addOption(sinceOption('since'))
+    .addOption(sinceOption('after'))
+    .parse();
+
+const { since } = program.opts();
 
 try {
     displayLatestCommits({
         from: process.cwd(),
-        since: null
+        since: since
     });
 } catch (e) {
     console.log(e);
